a43bd0ba400399d2d058bb81f2316041a94a40b1
Bayram Karahan <karahan_bayram42@hotmail.com>
2025-10-20 18:45:26 +0200

    add path -home -alumni -career -events -stories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Alumni } from './alumni/alumni';
+import { Career } from './career/career';
+import { Events } from './events/events';
+import { Stories } from './stories/stories';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'alumni', component: Alumni },
+  { path: 'career', component: Career },
+  { path: 'events', component: Events },
+  { path: 'stories', component: Stories },
+];
