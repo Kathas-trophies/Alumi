05a9d538036740867ebf91d72d38d12899d9e6ab
Mohab-S <mohabsalem52@gmail.com>
2025-10-22 11:45:40 +0200

    Add files via upload

---
+++ alumni/alumni.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumni',
+  imports: [],
+  templateUrl: './alumni.html',
+  styleUrl: './alumni.css',
+})
+export class Alumni {}
